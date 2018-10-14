d2cb234a2720f403583af08a8282dc96c27fa872
likun <likun@joynova.com.cn>
2018-10-14 14:31:51 +0800

    add readme file

--- netgo/tcp/wrapper_test.go
+++ netgo/tcp/wrapper_test.go
@@ -1,52 +1,58 @@
 package tcp
 
 import (
+	"encoding/binary"
+	"fmt"
+	"github.com/xlkness/netgo/event"
+	"github.com/xlkness/netgo/netgo"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestEcho(t *testing.T) {
-	//server := NewSocketTcpListener("127.0.0.1:9190", 1<<16, time.Minute, func(e *event.Event, client *SocketTcp) {
-	//	switch e.Type {
-	//	case event.EventTypeSocketConnect:
-	//		fmt.Printf("client connect!\n")
-	//	case event.EventTypeSocketClose:
-	//		fmt.Printf("client close!\n")
-	//	case event.EventTypeSocketRecv:
-	//		fmt.Printf("recv client msg:%v-%v\n", e.Msg.Tag, string(e.Msg.Payload))
-	//		buf := make([]byte, 8)
-	//		binary.LittleEndian.PutUint32(buf, uint32(123))
-	//		binary.LittleEndian.PutUint32(buf[4:], uint32(len(e.Msg.Payload)))
-	//		buf = append(buf, e.Msg.Payload...)
-	//		client.Write(buf)
-	//	}
-	//})
-	//server.StartListen()
-	//
-	//wg := &sync.WaitGroup{}
-	//wg.Add(10)
-	//for i := 0; i < 10; i++ {
-	//	go func() {
-	//		c := NewSocketTcpConnector("127.0.0.1:9190", 1<<16, time.Minute)
-	//		err := c.Connect()
-	//		if err != nil {
-	//			fmt.Printf("connect %s err:%v\n", c.addr, err)
-	//			return
-	//		}
-	//		payload := []byte("likun")
-	//		buf := make([]byte, 8)
-	//		binary.LittleEndian.PutUint32(buf, uint32(123))
-	//		binary.LittleEndian.PutUint32(buf[4:], uint32(len(payload)))
-	//		buf = append(buf, payload...)
-	//		c.Write(buf)
-	//		tag, msg, err := c.Read()
-	//		if err != nil {
-	//			fmt.Printf("write and recv err:%v", err)
-	//			return
-	//		}
-	//		fmt.Printf("recv echo msg:%v-%v\n", tag, string(msg))
-	//		c.Close()
-	//		wg.Done()
-	//	}()
-	//}
-	//wg.Wait()
+	server := NewSocketTcpListener("127.0.0.1:9190", 1<<16, time.Minute, func(e *event.Event, client netgo.Socket) {
+		switch e.Type {
+		case event.EventTypeSocketConnect:
+			fmt.Printf("client connect!\n")
+		case event.EventTypeSocketClose:
+			fmt.Printf("client close!\n")
+		case event.EventTypeSocketRecv:
+			fmt.Printf("recv client msg:%v-%v\n", e.Msg.Tag, string(e.Msg.Payload))
+			buf := make([]byte, 8)
+			binary.LittleEndian.PutUint32(buf, uint32(123))
+			binary.LittleEndian.PutUint32(buf[4:], uint32(len(e.Msg.Payload)))
+			buf = append(buf, e.Msg.Payload...)
+			client.Write(buf)
+		}
+	})
+	server.StartListen()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			c := NewSocketTcpConnector("127.0.0.1:9190", 1<<16, time.Minute)
+			err := c.Connect()
+			if err != nil {
+				fmt.Printf("connect %s err:%v\n", c.addr, err)
+				return
+			}
+			payload := []byte("likun")
+			buf := make([]byte, 8)
+			binary.LittleEndian.PutUint32(buf, uint32(123))
+			binary.LittleEndian.PutUint32(buf[4:], uint32(len(payload)))
+			buf = append(buf, payload...)
+			c.Write(buf)
+			tag, msg, err := c.Read()
+			if err != nil {
+				fmt.Printf("write and recv err:%v", err)
+				return
+			}
+			fmt.Printf("recv echo msg:%v-%v\n", tag, string(msg))
+			c.Close()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
--- netgo/udp/wrapper_test.go
+++ netgo/udp/wrapper_test.go
@@ -1,53 +1,59 @@
 package udp
 
 import (
+	"encoding/binary"
+	"fmt"
+	"github.com/xlkness/netgo/event"
+	"github.com/xlkness/netgo/netgo"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestEcho(t *testing.T) {
-	//wg := &sync.WaitGroup{}
-	//server := NewSocketUdpListener("127.0.0.1:9091", 1<<16, -1, func(e *event.Event, udpSocket *SocketUdp) {
-	//	switch e.Type {
-	//	case event.EventTypeSocketRecv:
-	//		fmt.Printf("recv msg:%v\n", string(e.Msg.Payload))
-	//		buf := make([]byte, 8)
-	//		binary.LittleEndian.PutUint32(buf, e.Msg.Tag)
-	//		binary.LittleEndian.PutUint32(buf[4:], uint32(len(e.Msg.Payload)))
-	//		buf = append(buf, e.Msg.Payload...)
-	//		err := udpSocket.Write(buf)
-	//		if err != nil {
-	//			fmt.Printf("echo write err:%v, %v\n", err, len(buf))
-	//		}
-	//	}
-	//})
-	//go server.StartListen()
-	//time.Sleep(1 * time.Second)
-	//wg.Add(10)
-	//for i := 0; i < 10; i++ {
-	//	go func() {
-	//		client := NewSocketUdpConnector("127.0.0.1:9091", 1<<16, -1)
-	//		if err := client.Connect(); err != nil {
-	//			fmt.Printf("conect err:%v\n", err)
-	//			return
-	//		}
-	//		payload := []byte("likun")
-	//		buf := make([]byte, 8)
-	//		binary.LittleEndian.PutUint32(buf, uint32(123))
-	//		binary.LittleEndian.PutUint32(buf[4:], uint32(len(payload)))
-	//		buf = append(buf, payload...)
-	//		err := client.Write(buf)
-	//		if err != nil {
-	//			fmt.Printf("write err:%v\n", err)
-	//			return
-	//		}
-	//		tag, payload, err := client.Read()
-	//		if err != nil {
-	//			fmt.Printf("recv err:%v\n", err)
-	//			return
-	//		}
-	//		fmt.Printf("recv echo msg:%v-%v\n", tag, string(payload))
-	//		wg.Done()
-	//	}()
-	//}
-	//wg.Wait()
+	wg := &sync.WaitGroup{}
+	server := NewSocketUdpListener("127.0.0.1:9091", 1<<16, -1, func(e *event.Event, client netgo.Socket) {
+		switch e.Type {
+		case event.EventTypeSocketRecv:
+			fmt.Printf("recv msg:%v\n", string(e.Msg.Payload))
+			buf := make([]byte, 8)
+			binary.LittleEndian.PutUint32(buf, e.Msg.Tag)
+			binary.LittleEndian.PutUint32(buf[4:], uint32(len(e.Msg.Payload)))
+			buf = append(buf, e.Msg.Payload...)
+			err := client.Write(buf)
+			if err != nil {
+				fmt.Printf("echo write err:%v, %v\n", err, len(buf))
+			}
+		}
+	})
+	go server.StartListen()
+	time.Sleep(1 * time.Second)
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			client := NewSocketUdpConnector("127.0.0.1:9091", 1<<16, -1)
+			if err := client.Connect(); err != nil {
+				fmt.Printf("conect err:%v\n", err)
+				return
+			}
+			payload := []byte("likun")
+			buf := make([]byte, 8)
+			binary.LittleEndian.PutUint32(buf, uint32(123))
+			binary.LittleEndian.PutUint32(buf[4:], uint32(len(payload)))
+			buf = append(buf, payload...)
+			err := client.Write(buf)
+			if err != nil {
+				fmt.Printf("write err:%v\n", err)
+				return
+			}
+			tag, payload, err := client.Read()
+			if err != nil {
+				fmt.Printf("recv err:%v\n", err)
+				return
+			}
+			fmt.Printf("recv echo msg:%v-%v\n", tag, string(payload))
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
